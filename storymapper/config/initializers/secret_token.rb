8c66909884ff9e23ec864ccfce8e6445d408aa3d
Christopher Bartling <chris.bartling@gmail.com>
2012-06-15 21:03:57 -0500

    Created Rails app.

---
+++ storymapper/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storymapper::Application.config.secret_token = 'bbd620571e79ed42268b0b0f00ff250f062265f13d5c6101e4d21293c328413354619472a43a82d3d2d29be6f87f5c0a8deba825f348253343d492bc462d3555'
